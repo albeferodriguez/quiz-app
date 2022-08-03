85afc273fb6f21e7b6bc0073de2b96640318b8f0
Alberto Fernández-Baíllo <alberto.fernandezb@i3television.es>
2022-08-03 17:13:44 +0200

    Created genreal culture questions

--- QuizApp/Shared/Features/QuizView/QuizPresenter.swift
+++ QuizApp/Shared/Features/QuizView/QuizPresenter.swift
@@ -163,4 +163,170 @@ class QuizPresenter {
             answer: 3
         )
     ]
+    
+    let cinemaQuestions = [
+        QuizEntity(
+            question: "¿Qué pelicula no dirigió David Fincher?",
+            answer1: AnswerEntity(id: 0, text: "Gone Girl"),
+            answer2: AnswerEntity(id: 1, text: "Alien Evolution"),
+            answer3: AnswerEntity(id: 2, text: "Zodiak"),
+            answer4: AnswerEntity(id: 3, text: "Memento"),
+            answer: 3
+        ),
+        QuizEntity(
+            question: "¿Cuántas películas ha dirigido Quentin Tarantino?",
+            answer1: AnswerEntity(id: 0, text: "9"),
+            answer2: AnswerEntity(id: 1, text: "8"),
+            answer3: AnswerEntity(id: 2, text: "7"),
+            answer4: AnswerEntity(id: 3, text: "6"),
+            answer: 0
+        )
+    ]
+    
+    let generalCultureList = [
+        QuizEntity(
+            question: "¿Quién escribió la Odisea?",
+            answer1: AnswerEntity(id: 0, text: "Homero"),
+            answer2: AnswerEntity(id: 1, text: "Aristóteles"),
+            answer3: AnswerEntity(id: 2, text: "Sócrates"),
+            answer4: AnswerEntity(id: 3, text: "Arturo Perez Reverte"),
+            answer: 0
+        ),
+        QuizEntity(
+            question: "¿En que año cayó el muro de Berlín?",
+            answer1: AnswerEntity(id: 0, text: "1990"),
+            answer2: AnswerEntity(id: 1, text: "1989"),
+            answer3: AnswerEntity(id: 2, text: "1988"),
+            answer4: AnswerEntity(id: 3, text: "1987"),
+            answer: 1
+        ),
+        QuizEntity(
+            question: "¿Dónde estaba la base estadounidense Pearl Harbor?",
+            answer1: AnswerEntity(id: 0, text: "Hawai"),
+            answer2: AnswerEntity(id: 1, text: "Taiwan"),
+            answer3: AnswerEntity(id: 2, text: "Iwo Jima"),
+            answer4: AnswerEntity(id: 3, text: "Japón"),
+            answer: 0
+        ),
+        QuizEntity(
+            question: "¿Quién pronunció la frase: Pienso, luego existo?",
+            answer1: AnswerEntity(id: 0, text: "Sócrates"),
+            answer2: AnswerEntity(id: 1, text: "Platón"),
+            answer3: AnswerEntity(id: 2, text: "Kant"),
+            answer4: AnswerEntity(id: 3, text: "Descartes"),
+            answer: 3
+        ),
+        QuizEntity(
+            question: "¿En qué año fue el golpe de estado de Tejero",
+            answer1: AnswerEntity(id: 0, text: "1979"),
+            answer2: AnswerEntity(id: 1, text: "1980"),
+            answer3: AnswerEntity(id: 2, text: "1981"),
+            answer4: AnswerEntity(id: 3, text: "1982"),
+            answer: 2
+        ),
+        QuizEntity(
+            question: "¿Qué animal causa mas muertes humanas al año?",
+            answer1: AnswerEntity(id: 0, text: "Garrapata"),
+            answer2: AnswerEntity(id: 1, text: "Pulga"),
+            answer3: AnswerEntity(id: 2, text: "Mosquito"),
+            answer4: AnswerEntity(id: 3, text: "Avista africana"),
+            answer: 2
+        ),
+        QuizEntity(
+            question: "¿Qué país produce más vino en el mundo?",
+            answer1: AnswerEntity(id: 0, text: "Italia"),
+            answer2: AnswerEntity(id: 1, text: "Francia"),
+            answer3: AnswerEntity(id: 2, text: "España"),
+            answer4: AnswerEntity(id: 3, text: "Portugal"),
+            answer: 0
+        ),
+        QuizEntity(
+            question: "¿Cuál fue el mayor descubrimiento de Albert Einstein?",
+            answer1: AnswerEntity(id: 0, text: "La teoría de cuerdas"),
+            answer2: AnswerEntity(id: 1, text: "La teoría de la gravedad"),
+            answer3: AnswerEntity(id: 2, text: "La teoría de la relatividad"),
+            answer4: AnswerEntity(id: 3, text: "La teoría de la gravedad"),
+            answer: 2
+        ),
+        QuizEntity(
+            question: "¿Cómo se llama la capital de Mongolia?",
+            answer1: AnswerEntity(id: 0, text: "Nicosia"),
+            answer2: AnswerEntity(id: 1, text: "Ottawa"),
+            answer3: AnswerEntity(id: 2, text: "Ulan Bator"),
+            answer4: AnswerEntity(id: 3, text: "Podgorica"),
+            answer: 2
+        ),
+        QuizEntity(
+            question: "¿Qué país empezó a utilizar papel moneda como billetes de pago?",
+            answer1: AnswerEntity(id: 0, text: "China"),
+            answer2: AnswerEntity(id: 1, text: "España"),
+            answer3: AnswerEntity(id: 2, text: "Alemania"),
+            answer4: AnswerEntity(id: 3, text: "Australia"),
+            answer: 0
+        ),
+        QuizEntity(
+            question: "¿Cuál es la película que ha recibido más Oscars?",
+            answer1: AnswerEntity(id: 0, text: "Ben-Hur"),
+            answer2: AnswerEntity(id: 1, text: "Lo que el viento se llevo"),
+            answer3: AnswerEntity(id: 2, text: "West Side Story"),
+            answer4: AnswerEntity(id: 3, text: "Amadeus"),
+            answer: 0
+        ),
+        QuizEntity(
+            question: "¿En qué famosa batalla combatió Miguel de Cervantes?",
+            answer1: AnswerEntity(id: 0, text: "Navas de Tolosa"),
+            answer2: AnswerEntity(id: 1, text: "Las Termópilas"),
+            answer3: AnswerEntity(id: 2, text: "Lepanto"),
+            answer4: AnswerEntity(id: 3, text: "Primera guerra mundial"),
+            answer: 2
+        ),
+        QuizEntity(
+            question: "¿Quién escribió la Divina Comedia?",
+            answer1: AnswerEntity(id: 0, text: "Dante Aligheri"),
+            answer2: AnswerEntity(id: 1, text: "Dante Alighier"),
+            answer3: AnswerEntity(id: 2, text: "Dante Allighieri"),
+            answer4: AnswerEntity(id: 3, text: "Dante Alighieri"),
+            answer: 3
+        ),
+        QuizEntity(
+            question: "¿Cómo se denomina al resultado de una división?",
+            answer1: AnswerEntity(id: 0, text: "Cociente"),
+            answer2: AnswerEntity(id: 1, text: "Coeficiente"),
+            answer3: AnswerEntity(id: 2, text: "Coecente"),
+            answer4: AnswerEntity(id: 3, text: "Dividendo"),
+            answer: 0
+        ),
+        QuizEntity(
+            question: "¿De dónde provienen los Juegos Olímpicos?",
+            answer1: AnswerEntity(id: 0, text: "Roma"),
+            answer2: AnswerEntity(id: 1, text: "Grecia"),
+            answer3: AnswerEntity(id: 2, text: "España"),
+            answer4: AnswerEntity(id: 3, text: "Asia"),
+            answer: 1
+        ),
+        QuizEntity(
+            question: "¿Quién dió la primera vuelta al mundo?",
+            answer1: AnswerEntity(id: 0, text: "Magallanes"),
+            answer2: AnswerEntity(id: 1, text: "Juan Sebastián Elcano"),
+            answer3: AnswerEntity(id: 2, text: "Magallanes y Juan Sebastian Elcano"),
+            answer4: AnswerEntity(id: 3, text: "Cristobal Colón"),
+            answer: 0
+        ),
+        QuizEntity(
+            question: "¿De dónde proviene la Ayahuasca?",
+            answer1: AnswerEntity(id: 0, text: "Amazonas"),
+            answer2: AnswerEntity(id: 1, text: "Bosques de Perú"),
+            answer3: AnswerEntity(id: 2, text: "Del Machupichu"),
+            answer4: AnswerEntity(id: 3, text: "Bosque del Nilo"),
+            answer: 0
+        ),
+        QuizEntity(
+            question: "¿Qué famoso filósofo clásico fue maestro de Alejando Magno?",
+            answer1: AnswerEntity(id: 0, text: "Socrates"),
+            answer2: AnswerEntity(id: 1, text: "Hermes"),
+            answer3: AnswerEntity(id: 2, text: "Platón"),
+            answer4: AnswerEntity(id: 3, text: "Aristóteles"),
+            answer: 3
+        ),
+    ]
 }
